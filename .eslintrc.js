848dcdb9693cff7547a5cde4fce38d015edc581a
bbenligiray <bbenligiray@gmail.com>
2023-03-10 21:24:24 +0300

    Remove ts

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,11 +4,9 @@ module.exports = {
     mocha: true,
     node: true,
   },
-  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 11,
     sourceType: 'module',
   },
   extends: 'eslint:recommended',
-  plugins: ['@typescript-eslint'],
 };
