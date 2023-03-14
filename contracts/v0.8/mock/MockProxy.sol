576fa6b08bf5a5fe727ba14c72f27ee41cd359f2
bbenligiray <bbenligiray@gmail.com>
2023-03-14 16:16:01 +0300

    Add a mock proxy contract

---
+++ contracts/v0.8/mock/MockProxy.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "../interfaces/IProxy.sol";
+
+// This contract is provided for testing purposes. It can be extended to mock
+// the interface of specific proxy types such as DataFeedProxy and DapiProxy.
+contract MockProxy is IProxy {
+    address public immutable override dapiServer;
+
+    int224 private _value;
+
+    uint32 private _timestamp;
+
+    constructor(address _dapiServer) {
+        dapiServer = _dapiServer;
+    }
+
+    function read()
+        external
+        view
+        override
+        returns (int224 value, uint32 timestamp)
+    {
+        (value, timestamp) = (_value, timestamp);
+    }
+
+    function mock(int224 value, uint32 timestamp) external {
+        (_value, timestamp) = (value, timestamp);
+    }
+}
