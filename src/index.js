c4718c5c7d71322c5bdeeb62a5c610b36f1bc1e3
bbenligiray <bbenligiray@gmail.com>
2023-03-16 19:46:32 +0300

    Add proxy address computation functions

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
-const { references } = require('@api3/airnode-protocol-v1');
+const ethers = require('ethers');
+const { references, DapiProxy__factory, DataFeedProxy__factory } = require('@api3/airnode-protocol-v1');
 
-module.exports = { references };
+function computeDapiProxyAddress(chainId, dapiName, metadata) {
+  const dapiNameHash = ethers.utils.solidityKeccak256(['bytes32'], [ethers.utils.formatBytes32String(dapiName)]);
+  const initcode = ethers.utils.solidityPack(
+    ['bytes', 'bytes'],
+    [
+      DapiProxy__factory.bytecode,
+      ethers.utils.defaultAbiCoder.encode(
+        ['address', 'bytes32'],
+        [references.Api3ServerV1[chainId.toString()], dapiNameHash]
+      ),
+    ]
+  );
+  return ethers.utils.getCreate2Address(
+    references.ProxyFactory[chainId],
+    ethers.utils.keccak256(metadata),
+    ethers.utils.keccak256(initcode)
+  );
+}
+
+function computeDataFeedProxyAddress(chainId, dataFeedId, metadata) {
+  const initcode = ethers.utils.solidityPack(
+    ['bytes', 'bytes'],
+    [
+      DataFeedProxy__factory.bytecode,
+      ethers.utils.defaultAbiCoder.encode(
+        ['address', 'bytes32'],
+        [references.Api3ServerV1[chainId.toString()], dataFeedId]
+      ),
+    ]
+  );
+  return ethers.utils.getCreate2Address(
+    references.ProxyFactory[chainId],
+    ethers.utils.keccak256(metadata),
+    ethers.utils.keccak256(initcode)
+  );
+}
+
+module.exports = { references, computeDapiProxyAddress, computeDataFeedProxyAddress };
