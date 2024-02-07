2fa854cc9db578cf9c701769ab6295ed5e877af0
Burak Benligiray <bbenligiray@gmail.com>
2024-02-07 17:20:32 +0300

    Skip bytecode verification if the deterministic deployment address matches

--- scripts/verify-deployments.ts
+++ scripts/verify-deployments.ts
@@ -12,7 +12,7 @@ import { config, deployments, ethers } from 'hardhat';
 
 import { chainsSupportedByDapis } from '../src/supported-chains';
 
-const METADATA_HASH_LENGTH = 106;
+const METADATA_HASH_LENGTH = 53 * 2;
 const CREATE2_FACTORY_ADDRESS = '0x4e59b44847b379578588920cA78FbF26c0B4956C';
 
 async function main() {
@@ -35,68 +35,70 @@ async function main() {
             deployment.args
           )
         : '0x';
+      const salt = ethers.ZeroHash;
+      const expectedDeterministicDeploymentAddress = ethers.getCreate2Address(
+        CREATE2_FACTORY_ADDRESS,
+        salt,
+        ethers.solidityPackedKeccak256(['bytes', 'bytes'], [artifact.bytecode, expectedEncodedConstructorArguments])
+      );
 
-      const goFetchCreationTx = await go(async () => provider.getTransaction(deployment.transactionHash), {
-        retries: 5,
-        attemptTimeoutMs: 10_000,
-        totalTimeoutMs: 50_000,
-        delay: {
-          type: 'random',
-          minDelayMs: 2000,
-          maxDelayMs: 5000,
-        },
-      });
-      if (!goFetchCreationTx.success || !goFetchCreationTx.data) {
-        throw new Error(`${chainAlias} ${contractName} creation tx could not be fetched`);
-      }
-      const creationTx: any = goFetchCreationTx.data;
-      const creationData = creationTx.data;
-
-      const deterministicDeployment = creationTx!.to === CREATE2_FACTORY_ADDRESS;
-
-      if (deterministicDeployment) {
-        const salt = ethers.ZeroHash;
-        const deterministicDeploymentAddress = ethers.getCreate2Address(
-          CREATE2_FACTORY_ADDRESS,
-          salt,
-          ethers.solidityPackedKeccak256(['bytes', 'bytes'], [artifact.bytecode, expectedEncodedConstructorArguments])
-        );
-        if (deployment.address !== deterministicDeploymentAddress) {
-          throw new Error(`${chainAlias} ${contractName} deterministic deployment address does not match`);
+      if (deployment.address === expectedDeterministicDeploymentAddress) {
+        const goFetchContractCode = await go(async () => provider.getCode(deployment.address), {
+          retries: 5,
+          attemptTimeoutMs: 10_000,
+          totalTimeoutMs: 50_000,
+          delay: {
+            type: 'random',
+            minDelayMs: 2000,
+            maxDelayMs: 5000,
+          },
+        });
+        if (!goFetchContractCode.success || !goFetchContractCode.data) {
+          throw new Error(`${chainAlias} ${contractName} (deterministic) contract code could not be fetched`);
         }
-      } else if (deployment.address !== ethers.getCreateAddress(creationTx)) {
-        throw new Error(`${chainAlias} ${contractName} deployment address does not match`);
-      }
+        if (goFetchContractCode.data === '0x') {
+          throw new Error(`${chainAlias} ${contractName} (deterministic) contract code does not exist`);
+        }
+      } else {
+        const goFetchCreationTx = await go(async () => provider.getTransaction(deployment.transactionHash), {
+          retries: 5,
+          attemptTimeoutMs: 10_000,
+          totalTimeoutMs: 50_000,
+          delay: {
+            type: 'random',
+            minDelayMs: 2000,
+            maxDelayMs: 5000,
+          },
+        });
+        if (!goFetchCreationTx.success || !goFetchCreationTx.data) {
+          throw new Error(`${chainAlias} ${contractName} creation tx could not be fetched`);
+        }
+        const creationTx: any = goFetchCreationTx.data;
+        const creationData = creationTx.data;
 
-      // Salt precedes the bytecode if it is a deterministic deployment
-      const creationBytecode = deterministicDeployment
-        ? creationData.slice(0, artifact.bytecode.length + 64)
-        : creationData.slice(0, artifact.bytecode.length);
-      const creationBytecodeWithoutMetadataHash = creationBytecode.slice(0, -METADATA_HASH_LENGTH);
-      const creationMetadataHash = `0x${creationBytecode.slice(-METADATA_HASH_LENGTH)}`;
-      const creationEncodedConstructorArguments = `0x${creationData.slice(creationBytecode.length)}`;
+        if (deployment.address !== ethers.getCreateAddress(creationTx)) {
+          throw new Error(`${chainAlias} ${contractName} creation tx deployment address does not match`);
+        }
 
-      const expectedCreationBytecode = deterministicDeployment
-        ? `${ethers.ZeroHash}${artifact.bytecode.slice(2)}`
-        : artifact.bytecode;
-      const expectedBytecodeWithoutMetadataHash = expectedCreationBytecode.slice(0, -METADATA_HASH_LENGTH);
-      const expectedMetadataHash = `0x${expectedCreationBytecode.slice(-METADATA_HASH_LENGTH)}`;
+        const creationBytecode = creationData.slice(0, artifact.bytecode.length);
+        const creationBytecodeWithoutMetadataHash = creationBytecode.slice(0, -METADATA_HASH_LENGTH);
+        const creationMetadataHash = `0x${creationBytecode.slice(-METADATA_HASH_LENGTH)}`;
+        const creationEncodedConstructorArguments = `0x${creationData.slice(creationBytecode.length)}`;
 
-      if (creationBytecodeWithoutMetadataHash !== expectedBytecodeWithoutMetadataHash) {
-        throw new Error(
-          `${chainAlias} ${contractName} ${deterministicDeployment ? 'deterministic' : ''} deployment bytecode does not match`
-        );
-      }
-      if (creationMetadataHash !== expectedMetadataHash) {
-        // eslint-disable-next-line no-console
-        console.log(
-          `${chainAlias} ${contractName} ${deterministicDeployment ? 'deterministic' : ''} deployment metadata hash does not match`
-        );
-      }
-      if (creationEncodedConstructorArguments !== expectedEncodedConstructorArguments) {
-        throw new Error(
-          `${chainAlias} ${contractName} ${deterministicDeployment ? 'deterministic' : ''} deployment constructor arguments do not match`
-        );
+        const expectedCreationBytecode = artifact.bytecode;
+        const expectedBytecodeWithoutMetadataHash = expectedCreationBytecode.slice(0, -METADATA_HASH_LENGTH);
+        const expectedMetadataHash = `0x${expectedCreationBytecode.slice(-METADATA_HASH_LENGTH)}`;
+
+        if (creationBytecodeWithoutMetadataHash !== expectedBytecodeWithoutMetadataHash) {
+          throw new Error(`${chainAlias} ${contractName} deployment bytecode does not match`);
+        }
+        if (creationMetadataHash !== expectedMetadataHash) {
+          // eslint-disable-next-line no-console
+          console.log(`${chainAlias} ${contractName} deployment metadata hash does not match`);
+        }
+        if (creationEncodedConstructorArguments !== expectedEncodedConstructorArguments) {
+          throw new Error(`${chainAlias} ${contractName} deployment constructor arguments do not match`);
+        }
       }
     }
   }
