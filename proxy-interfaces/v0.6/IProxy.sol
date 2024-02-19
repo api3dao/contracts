64c360c70b9e086b020e6cf77354015154a1ee0c
Burak Benligiray <bbenligiray@gmail.com>
2024-02-19 11:32:56 +0300

    Update contract paths in the package

--- proxy-interfaces/v0.6/IProxy.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.6.0;
-
-interface IProxy {
-    function api3ServerV1() external view returns (address);
-
-    function read() external view returns (int224 value, uint32 timestamp);
-}
--- proxy-interfaces/v0.8/IProxy.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-interface IProxy {
-    function api3ServerV1() external view returns (address);
-
-    function read() external view returns (int224 value, uint32 timestamp);
-}
