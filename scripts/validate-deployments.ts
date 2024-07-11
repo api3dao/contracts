8ca2a82889633c98ecd898a67b492963fe1c7e7d
Burak Benligiray <bbenligiray@gmail.com>
2024-07-11 10:20:15 +0300

    Do not let chain-level errors stop verification and validation

--- scripts/validate-deployments.ts
+++ scripts/validate-deployments.ts
@@ -13,98 +13,65 @@ import {
 import managerMultisigAddresses from '../data/manager-multisig.json';
 import type { AccessControlRegistry, OwnableCallForwarder } from '../src/index';
 
-async function main() {
-  const networks = process.env.NETWORK
-    ? [process.env.NETWORK]
-    : new Set([
-        ...Object.keys(managerMultisigAddresses),
-        ...chainsSupportedByDapis,
-        ...chainsSupportedByMarket,
-        ...chainsSupportedByOevAuctions,
-      ]);
-
-  for (const network of networks) {
-    if (Object.keys(managerMultisigAddresses).includes(network)) {
-      const provider = new ethers.JsonRpcProvider((config.networks[network] as any).url);
-      // Validate that the OwnableCallForwarder owner is the manager multisig
-      const { address: ownableCallForwarderAddress, abi: ownableCallForwarderAbi } = JSON.parse(
-        fs.readFileSync(join('deployments', network, `OwnableCallForwarder.json`), 'utf8')
+async function validateDeployments(network: string) {
+  if (Object.keys(managerMultisigAddresses).includes(network)) {
+    const provider = new ethers.JsonRpcProvider((config.networks[network] as any).url);
+    // Validate that the OwnableCallForwarder owner is the manager multisig
+    const { address: ownableCallForwarderAddress, abi: ownableCallForwarderAbi } = JSON.parse(
+      fs.readFileSync(join('deployments', network, `OwnableCallForwarder.json`), 'utf8')
+    );
+    const ownableCallForwarder = new ethers.Contract(
+      ownableCallForwarderAddress,
+      ownableCallForwarderAbi,
+      provider
+    ) as unknown as OwnableCallForwarder;
+    const goFetchOwnableCallForwarderOwner = await go(async () => ownableCallForwarder.owner(), {
+      retries: 5,
+      attemptTimeoutMs: 10_000,
+      totalTimeoutMs: 50_000,
+      delay: {
+        type: 'random',
+        minDelayMs: 2000,
+        maxDelayMs: 5000,
+      },
+    });
+    if (!goFetchOwnableCallForwarderOwner.success || !goFetchOwnableCallForwarderOwner.data) {
+      throw new Error(`${network} OwnableCallForwarder owner could not be fetched`);
+    }
+    if (
+      goFetchOwnableCallForwarderOwner.data.toLowerCase() !==
+      managerMultisigAddresses[network as keyof typeof managerMultisigAddresses].toLowerCase()
+    ) {
+      throw new Error(
+        `${network} OwnableCallForwarder owner ${goFetchOwnableCallForwarderOwner.data.toLowerCase()} is not the same as the manager multisig address ${managerMultisigAddresses[network as keyof typeof managerMultisigAddresses].toLowerCase()}`
       );
-      const ownableCallForwarder = new ethers.Contract(
-        ownableCallForwarderAddress,
-        ownableCallForwarderAbi,
+    }
+    if (chainsSupportedByMarket.includes(network)) {
+      // Validate that ExternalMulticallSimulator and Api3Market are dAPI name setters
+      const rootRole = ethers.solidityPackedKeccak256(['address'], [ownableCallForwarderAddress]);
+      const adminRole = ethers.solidityPackedKeccak256(
+        ['bytes32', 'bytes32'],
+        [rootRole, ethers.solidityPackedKeccak256(['string'], ['Api3ServerV1 admin'])]
+      );
+      const dapiNameSetterRole = ethers.solidityPackedKeccak256(
+        ['bytes32', 'bytes32'],
+        [adminRole, ethers.solidityPackedKeccak256(['string'], ['dAPI name setter'])]
+      );
+      const { address: accessControlRegistryAddress, abi: accessControlRegistryAbi } = JSON.parse(
+        fs.readFileSync(join('deployments', network, `AccessControlRegistry.json`), 'utf8')
+      );
+      const accessControlRegistry = new ethers.Contract(
+        accessControlRegistryAddress,
+        accessControlRegistryAbi,
         provider
-      ) as unknown as OwnableCallForwarder;
-      const goFetchOwnableCallForwarderOwner = await go(async () => ownableCallForwarder.owner(), {
-        retries: 5,
-        attemptTimeoutMs: 10_000,
-        totalTimeoutMs: 50_000,
-        delay: {
-          type: 'random',
-          minDelayMs: 2000,
-          maxDelayMs: 5000,
-        },
-      });
-      if (!goFetchOwnableCallForwarderOwner.success || !goFetchOwnableCallForwarderOwner.data) {
-        throw new Error(`${network} OwnableCallForwarder owner could not be fetched`);
-      }
-      if (
-        goFetchOwnableCallForwarderOwner.data.toLowerCase() !==
-        managerMultisigAddresses[network as keyof typeof managerMultisigAddresses].toLowerCase()
-      ) {
-        throw new Error(
-          `${network} OwnableCallForwarder owner ${goFetchOwnableCallForwarderOwner.data.toLowerCase()} is not the same as the manager multisig address ${managerMultisigAddresses[network as keyof typeof managerMultisigAddresses].toLowerCase()}`
+      ) as unknown as AccessControlRegistry;
+      const isTestnet = CHAINS.find((chain) => chain.alias === network)?.testnet;
+      if (!isTestnet) {
+        const { address: externalMulticallSimulatorAddress } = JSON.parse(
+          fs.readFileSync(join('deployments', network, `ExternalMulticallSimulator.json`), 'utf8')
         );
-      }
-      if (chainsSupportedByMarket.includes(network)) {
-        // Validate that ExternalMulticallSimulator and Api3Market are dAPI name setters
-        const rootRole = ethers.solidityPackedKeccak256(['address'], [ownableCallForwarderAddress]);
-        const adminRole = ethers.solidityPackedKeccak256(
-          ['bytes32', 'bytes32'],
-          [rootRole, ethers.solidityPackedKeccak256(['string'], ['Api3ServerV1 admin'])]
-        );
-        const dapiNameSetterRole = ethers.solidityPackedKeccak256(
-          ['bytes32', 'bytes32'],
-          [adminRole, ethers.solidityPackedKeccak256(['string'], ['dAPI name setter'])]
-        );
-        const { address: accessControlRegistryAddress, abi: accessControlRegistryAbi } = JSON.parse(
-          fs.readFileSync(join('deployments', network, `AccessControlRegistry.json`), 'utf8')
-        );
-        const accessControlRegistry = new ethers.Contract(
-          accessControlRegistryAddress,
-          accessControlRegistryAbi,
-          provider
-        ) as unknown as AccessControlRegistry;
-        const isTestnet = CHAINS.find((chain) => chain.alias === network)?.testnet;
-        if (!isTestnet) {
-          const { address: externalMulticallSimulatorAddress } = JSON.parse(
-            fs.readFileSync(join('deployments', network, `ExternalMulticallSimulator.json`), 'utf8')
-          );
-          const goFetchExternalMulticallSimulatorDapiNameSetterRoleStatus = await go(
-            async () => accessControlRegistry.hasRole(dapiNameSetterRole, externalMulticallSimulatorAddress),
-            {
-              retries: 5,
-              attemptTimeoutMs: 10_000,
-              totalTimeoutMs: 50_000,
-              delay: {
-                type: 'random',
-                minDelayMs: 2000,
-                maxDelayMs: 5000,
-              },
-            }
-          );
-          if (!goFetchExternalMulticallSimulatorDapiNameSetterRoleStatus.success) {
-            throw new Error(`${network} ExternalMulticallSimulator dAPI name setter role status could not be fetched`);
-          }
-          if (!goFetchExternalMulticallSimulatorDapiNameSetterRoleStatus.data) {
-            throw new Error(`${network} ExternalMulticallSimulator does not have the dAPI name setter role`);
-          }
-        }
-        const { address: api3MarketAddress } = JSON.parse(
-          fs.readFileSync(join('deployments', network, `Api3Market.json`), 'utf8')
-        );
-        const goFetchApi3MarketDapiNameSetterRoleStatus = await go(
-          async () => accessControlRegistry.hasRole(dapiNameSetterRole, api3MarketAddress),
+        const goFetchExternalMulticallSimulatorDapiNameSetterRoleStatus = await go(
+          async () => accessControlRegistry.hasRole(dapiNameSetterRole, externalMulticallSimulatorAddress),
           {
             retries: 5,
             attemptTimeoutMs: 10_000,
@@ -116,14 +83,68 @@ async function main() {
             },
           }
         );
-        if (!goFetchApi3MarketDapiNameSetterRoleStatus.success) {
-          throw new Error(`${network} Api3Market dAPI name setter role status could not be fetched`);
+        if (!goFetchExternalMulticallSimulatorDapiNameSetterRoleStatus.success) {
+          throw new Error(`${network} ExternalMulticallSimulator dAPI name setter role status could not be fetched`);
         }
-        if (!goFetchApi3MarketDapiNameSetterRoleStatus.data) {
-          throw new Error(`${network} Api3Market does not have the dAPI name setter role`);
+        if (!goFetchExternalMulticallSimulatorDapiNameSetterRoleStatus.data) {
+          throw new Error(`${network} ExternalMulticallSimulator does not have the dAPI name setter role`);
         }
       }
+      const { address: api3MarketAddress } = JSON.parse(
+        fs.readFileSync(join('deployments', network, `Api3Market.json`), 'utf8')
+      );
+      const goFetchApi3MarketDapiNameSetterRoleStatus = await go(
+        async () => accessControlRegistry.hasRole(dapiNameSetterRole, api3MarketAddress),
+        {
+          retries: 5,
+          attemptTimeoutMs: 10_000,
+          totalTimeoutMs: 50_000,
+          delay: {
+            type: 'random',
+            minDelayMs: 2000,
+            maxDelayMs: 5000,
+          },
+        }
+      );
+      if (!goFetchApi3MarketDapiNameSetterRoleStatus.success) {
+        throw new Error(`${network} Api3Market dAPI name setter role status could not be fetched`);
+      }
+      if (!goFetchApi3MarketDapiNameSetterRoleStatus.data) {
+        throw new Error(`${network} Api3Market does not have the dAPI name setter role`);
+      }
     }
+  }
+}
+
+async function main() {
+  const networks = process.env.NETWORK
+    ? [process.env.NETWORK]
+    : [
+        ...new Set([
+          ...Object.keys(managerMultisigAddresses),
+          ...chainsSupportedByDapis,
+          ...chainsSupportedByMarket,
+          ...chainsSupportedByOevAuctions,
+        ]),
+      ];
+
+  const erroredNetworks: string[] = [];
+  await Promise.all(
+    networks.map(async (network) => {
+      try {
+        await validateDeployments(network);
+      } catch (error) {
+        erroredNetworks.push(network);
+        // eslint-disable-next-line no-console
+        console.error(error, '\n');
+      }
+    })
+  );
+  if (erroredNetworks.length > 0) {
+    // eslint-disable-next-line no-console
+    console.error(`Validation failed on: ${erroredNetworks.join(', ')}`);
+    // eslint-disable-next-line unicorn/no-process-exit
+    process.exit(1);
   }
 }
 
