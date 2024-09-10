47dcb1d63147109a91e9bd906ad50114d88cd92c
Burak Benligiray <bbenligiray@gmail.com>
2024-09-10 20:40:22 +0300

    Remove Api3Market-related code

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,3 @@ export * from '../typechain-types';
 import deploymentAddresses from '../deployments/addresses.json';
 import managerMultisigAddresses from '../deployments/manager-multisig-addresses.json';
 export { deploymentAddresses, managerMultisigAddresses };
-export * from './market';
