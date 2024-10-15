94a57bf6b9eab48eb1404d5a2566c438a88baaae
Burak Benligiray <bbenligiray@gmail.com>
2024-10-15 11:58:07 +0300

    Do not require the expected deployer to sign the initial updates

--- deploy/2_update_beacon_set.ts
+++ deploy/2_update_beacon_set.ts
@@ -1,37 +1,9 @@
 import { type HardhatEthersSigner } from '@nomicfoundation/hardhat-ethers/signers';
-import type { AddressLike, BytesLike } from 'ethers';
+import type { BytesLike } from 'ethers';
 import { deployments, ethers, network } from 'hardhat';
 
 import { chainsSupportedByDapis } from '../data/chain-support.json';
-import { type Api3ServerV1, Api3ServerV1__factory } from '../src/index';
-
-type UpdateBeaconWithSignedDataArgsType = [AddressLike, BytesLike, number, BytesLike, BytesLike];
-
-async function getCallData(
-  api3ServerV1: Api3ServerV1,
-  airnodeAddress: AddressLike,
-  value: number,
-  templateId: BytesLike,
-  signature: BytesLike
-): Promise<BytesLike> {
-  const data = `0x${value.toString().padStart(64, '0')}` as BytesLike;
-  const args: UpdateBeaconWithSignedDataArgsType = [airnodeAddress, templateId, value, data, signature];
-  return api3ServerV1.interface.encodeFunctionData('updateBeaconWithSignedData', args);
-}
-
-function getUpdateSignature(ind: number): BytesLike {
-  const updateSignatures = [
-    '0x3149d2642658ca2bbf08a47bce005ef5edb8d6c084a154638f4acff14bd9dc77110b54243126ed6cea05eaf810d825b5b4d816e0c254f6160ff97abab67919ee1b',
-    '0x4da679af6915f03bf9c2865cedba81ac141c9ed40952fba7a9f827b3fba0236949d9bf3a3a468eb7a146f95369aed60998d0123afbb6260427d3e9140c8fbeda1b',
-    '0xb44989a9afb4b0f1a7cc21964c8f3bdfcf42f707823ab40771a473f3d2b8288468a1677d5c509233362ca73ef9e17653216ea72cff163a1a3246428fb02410971b',
-    '0xbbb946b391ac46ccf0bbd5f549a01cfcb36471525bfd80e8eef8bc62ea5ee1215adb7a4b3aa6a22cc9db3ac44837c765330d7660b1136edd0fdc30ec1a8a48ce1c',
-    '0x620db08a26b8acba0108a4e6be834aa5aefc8a7304581b2117ed775df40aedbf75ae190d7f000dd152f921df44469690b816834e68eef2bac2ea6daa8e94960b1b',
-    '0x94f32241043cda7efbb3aeff88e460eb46bce8d6b8df784621cd47a39790f7e80f7aca6567b2e656cdf4fd7277d3fd4522bafbfaeb2c31b6e837dd71af7869931c',
-    '0x2c25ed17835336f80d1a4705817685069e3d1b93108469e330766ae6921d27262a66ee6318e6aec3d58ce5fcfcd12fa3a528f2a3e042bbda6c9eba7b322099a01c',
-  ] as BytesLike[];
-
-  return updateSignatures[ind] as BytesLike;
-}
+import { Api3ServerV1__factory } from '../src/index';
 
 async function signMessage(value: number, templateId: BytesLike, deployer: HardhatEthersSigner): Promise<BytesLike> {
   const data = `0x${value.toString().padStart(64, '0')}` as BytesLike;
@@ -69,8 +41,6 @@ module.exports = async () => {
     return;
   }
 
-  const isExpectedDeployer = EXPECTED_DEPLOYER_ADDRESS === deployer!.address;
-
   const Api3ServerV1 = await deployments.get('Api3ServerV1');
   const api3ServerV1 = Api3ServerV1__factory.connect(Api3ServerV1.address, deployer!);
 
@@ -90,13 +60,37 @@ module.exports = async () => {
 
   const updateMulticallData = [] as BytesLike[];
 
+  /*
+  If the signer is the expected deployer, initial update signatures are created as such
   for (const [ind, templateId] of templateIds.entries()) {
     if (dataFeedReadings[ind] === initialValue) {
-      const signature = isExpectedDeployer
-        ? await signMessage(ind + 1, templateId, deployer!)
-        : getUpdateSignature(ind);
+      const signature = await signMessage(ind + 1, templateId, deployer!);
+      ...
+    }
+  }
+  We will use hardcoded initial update signatures below
+  */
+
+  const initialUpdateSignatures: BytesLike[] = [
+    '0x3149d2642658ca2bbf08a47bce005ef5edb8d6c084a154638f4acff14bd9dc77110b54243126ed6cea05eaf810d825b5b4d816e0c254f6160ff97abab67919ee1b',
+    '0x4da679af6915f03bf9c2865cedba81ac141c9ed40952fba7a9f827b3fba0236949d9bf3a3a468eb7a146f95369aed60998d0123afbb6260427d3e9140c8fbeda1b',
+    '0xb44989a9afb4b0f1a7cc21964c8f3bdfcf42f707823ab40771a473f3d2b8288468a1677d5c509233362ca73ef9e17653216ea72cff163a1a3246428fb02410971b',
+    '0xbbb946b391ac46ccf0bbd5f549a01cfcb36471525bfd80e8eef8bc62ea5ee1215adb7a4b3aa6a22cc9db3ac44837c765330d7660b1136edd0fdc30ec1a8a48ce1c',
+    '0x620db08a26b8acba0108a4e6be834aa5aefc8a7304581b2117ed775df40aedbf75ae190d7f000dd152f921df44469690b816834e68eef2bac2ea6daa8e94960b1b',
+    '0x94f32241043cda7efbb3aeff88e460eb46bce8d6b8df784621cd47a39790f7e80f7aca6567b2e656cdf4fd7277d3fd4522bafbfaeb2c31b6e837dd71af7869931c',
+    '0x2c25ed17835336f80d1a4705817685069e3d1b93108469e330766ae6921d27262a66ee6318e6aec3d58ce5fcfcd12fa3a528f2a3e042bbda6c9eba7b322099a01c',
+  ];
+
+  for (const [ind, templateId] of templateIds.entries()) {
+    if (dataFeedReadings[ind] === initialValue) {
       updateMulticallData.push(
-        await getCallData(api3ServerV1, EXPECTED_DEPLOYER_ADDRESS, ind + 1, templateId, signature)
+        api3ServerV1.interface.encodeFunctionData('updateBeaconWithSignedData', [
+          EXPECTED_DEPLOYER_ADDRESS,
+          templateId,
+          ind + 1,
+          `0x${(ind + 1).toString().padStart(64, '0')}`,
+          initialUpdateSignatures[ind]!,
+        ])
       );
     }
   }
@@ -113,7 +107,7 @@ module.exports = async () => {
     log('Beacon set update already executed');
   }
 
-  if (!isExpectedDeployer) {
+  if (EXPECTED_DEPLOYER_ADDRESS !== deployer!.address) {
     log('Skipping gas estimation for non-expected deployer');
     return;
   }
@@ -123,7 +117,13 @@ module.exports = async () => {
   for (const [ind, templateId] of templateIds.entries()) {
     const signature = await signMessage(ind + 102, templateId, deployer!);
     estimateGasMulticallData.push(
-      await getCallData(api3ServerV1, EXPECTED_DEPLOYER_ADDRESS, ind + 102, templateId, signature)
+      api3ServerV1.interface.encodeFunctionData('updateBeaconWithSignedData', [
+        EXPECTED_DEPLOYER_ADDRESS,
+        templateId,
+        ind + 1,
+        `0x${(ind + 102).toString().padStart(64, '0')}`,
+        signature,
+      ])
     );
   }
 
