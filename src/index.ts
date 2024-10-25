f03891894f554c1eae16b8719b1d2cde552fbd23
Burak Benligiray <bbenligiray@gmail.com>
2024-10-25 17:24:33 +0300

    Export auctioneer metadata

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,5 @@ export * from '../typechain-types';
 import deploymentAddresses from '../deployments/addresses.json';
 export { deploymentAddresses };
 export * from './proxy';
+import auctioneerMetadata from '../data/auctioneer-metadata.json';
+export { auctioneerMetadata };
