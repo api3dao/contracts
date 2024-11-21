0d9ab110d997e629f288ac18eb78f9723f9ceb4d
Burak Benligiray <bbenligiray@gmail.com>
2024-11-21 11:16:00 +0300

    Only keep the list of dApps, not what chains they are on

--- src/types.ts
+++ src/types.ts
@@ -1,10 +1,7 @@
 import { z } from 'zod';
 
 export const dappSchema = z.object({
-  id: z.string().regex(/^\d+$/),
-  name: z.string(),
-  chainId: z.string().regex(/^\d+$/),
-  url: z.string().url().optional(),
+  alias: z.string(),
 });
 
 export type Dapp = z.infer<typeof dappSchema>;
