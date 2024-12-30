40dacdb0691ce25d26ceb3836d0f242c8d9cc732
Burak Benligiray <bbenligiray@gmail.com>
2024-12-30 12:30:54 +0300

    Format

--- contracts/mock/MockApi3ReaderProxy.sol
+++ contracts/mock/MockApi3ReaderProxy.sol
@@ -9,8 +9,15 @@ import "../interfaces/IApi3ReaderProxy.sol";
 /// access control, it is only recommended to be used for local tests.
 contract MockApi3ReaderProxy is IApi3ReaderProxy {
     int224 private _value;
-
     uint32 private _timestamp;
+
+    /// @notice Mocks the value and timestamp of the API3 data feed represented
+    /// by this proxy contract
+    /// @param value Data feed value
+    /// @param timestamp Data feed timestamp
+    function mock(int224 value, uint32 timestamp) external {
+        (_value, _timestamp) = (value, timestamp);
+    }
 
     /// @notice Returns the mocked value and timestamp of the API3 data feed
     /// represented by this proxy contract
@@ -23,13 +30,5 @@ contract MockApi3ReaderProxy is IApi3ReaderProxy {
         returns (int224 value, uint32 timestamp)
     {
         (value, timestamp) = (_value, _timestamp);
-    }
-
-    /// @notice Mocks the value and timestamp of the API3 data feed represented
-    /// by this proxy contract
-    /// @param value Data feed value
-    /// @param timestamp Data feed timestamp
-    function mock(int224 value, uint32 timestamp) external {
-        (_value, _timestamp) = (value, timestamp);
     }
 }
