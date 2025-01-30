27ad0723fdd8c64e5961546ab2555ba882dfe4bf
hiletmis <31757771+hiletmis@users.noreply.github.com>
2025-01-30 15:32:26 +0300

    Remove 'lukso' from skippedChainAliases in OevAuctionHouse native currency rate validation

--- scripts/constants.ts
+++ scripts/constants.ts
@@ -11,4 +11,4 @@ export const goAsyncOptions: GoAsyncOptions = {
   },
 };
 
-export const skippedChainAliasesInOevAuctionHouseNativeCurrencyRateValidation = ['conflux', 'hashkey', 'lukso'];
+export const skippedChainAliasesInOevAuctionHouseNativeCurrencyRateValidation = ['conflux', 'hashkey'];
