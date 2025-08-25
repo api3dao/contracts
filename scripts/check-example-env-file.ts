a557f778dabb361bed0c9a4e14acc6a2ecd2fab5
bbenligiray <bbenligiray@gmail.com>
2025-08-25 19:57:38 +0300

    Base check-example-env-file on write-example-env-file

--- scripts/check-example-env-file.ts
+++ scripts/check-example-env-file.ts
@@ -1,57 +1,18 @@
 import * as fs from 'node:fs';
-import path from 'node:path';
 
-import { CHAINS } from '../src/generated/chains';
-import { type Chain } from '../src/types';
-import { toUpperSnakeCase } from '../src/utils/strings';
+import { hardhatConfig } from '../src/index';
 
-const INPUT_DIR = path.join('data', 'chains');
+const expectedEnvFileContents = hardhatConfig
+  .getEnvVariableNames()
+  .reduce((fileContents: string, envVariableName: string) => {
+    if (!envVariableName.startsWith('HARDHAT_HTTP_RPC_URL_')) {
+      return `${fileContents}${envVariableName}=\n`;
+    }
+    return fileContents;
+  }, '');
 
-const fileNames = fs.readdirSync(INPUT_DIR);
-const jsonFiles = fileNames.filter((fileName) => fileName.endsWith('.json'));
+const exampleEnvFileContents = fs.readFileSync('example.env', 'utf8');
 
-const logs: string[] = [];
-
-const jsonChains: Chain[] = jsonFiles.map((filePath: string) => {
-  const fullPath = path.join(INPUT_DIR, filePath);
-  const fileContentRaw = fs.readFileSync(fullPath, 'utf8');
-  return JSON.parse(fileContentRaw);
-});
-
-const chainsMap = new Map(CHAINS.map((chain) => [chain.alias, chain]));
-const jsonChainsMap = new Map(jsonChains.map((chain) => [chain.alias, chain]));
-
-const chainAliases = new Set(chainsMap.keys());
-const jsonAliases = new Set(jsonChainsMap.keys());
-
-const missingInChains = [...jsonAliases].filter((alias) => !chainAliases.has(alias));
-if (missingInChains.length > 0) {
-  logs.push(`Missing in CHAINS: ${missingInChains.join(', ')}\n`);
+if (exampleEnvFileContents !== expectedEnvFileContents) {
+  throw new Error(`example.env is outdated`);
 }
-const missingInJson = [...chainAliases].filter((alias) => !jsonAliases.has(alias));
-if (missingInJson.length > 0) {
-  logs.push(`Missing in JSON files: ${missingInJson.join(', ')}\n`);
-}
-
-const apiKeyEnvNames = jsonChains
-  .filter((chain) => chain.explorer?.api?.key?.required)
-  .map((chain) => `ETHERSCAN_API_KEY_${toUpperSnakeCase(chain.alias)}`);
-
-const expectedEnvVars = ['MNEMONIC', ...apiKeyEnvNames];
-const expectedEnvFile = expectedEnvVars.reduce((fileContents: string, envVariableName: string) => {
-  return `${fileContents}${envVariableName}=\n`;
-}, '');
-
-const exampleEnvFile = fs.readFileSync('example.env', 'utf8');
-
-if (exampleEnvFile !== expectedEnvFile) {
-  logs.push('Please update example.env running "pnpm write-example-env-file"');
-}
-
-if (logs.length > 0) {
-  // eslint-disable-next-line no-console
-  logs.forEach((log) => console.error(log));
-  process.exit(1);
-}
-
-process.exit(0);
