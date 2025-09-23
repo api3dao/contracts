0c43a1ea43bb0af835374fffbca1d6ec0d837f38
hiletmis <31757771+hiletmis@users.noreply.github.com>
2025-09-23 11:48:40 +0300

    Streamline check example env script

--- scripts/check-example-env-file.ts
+++ scripts/check-example-env-file.ts
@@ -1,8 +1,6 @@
 import * as fs from 'node:fs';
 
-const apiKeyEnvName = 'ETHERSCAN_API_KEY';
-
-const expectedEnvVars = ['MNEMONIC', apiKeyEnvName];
+const expectedEnvVars = ['MNEMONIC', 'ETHERSCAN_API_KEY'];
 const expectedExampleEnvFileContents = expectedEnvVars.reduce((fileContents: string, envVariableName: string) => {
   return `${fileContents}${envVariableName}=\n`;
 }, '');
